b7ffe4ffadd11de7a71995d8414b76daee2d3fb9
Graham Quirk <gquirk@valenciacollege.edu>
2022-10-15 19:13:43 -0400

    wrong files pushed

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,5 +1,6 @@
 package application;
 	
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
@@ -10,43 +11,112 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
 import javafx.scene.layout.BorderPane;
-import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.StackPane;
 
 
 public class Main extends Application {
+	
+	Button button;
+	TextArea textArea;
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-
-			Label labelOutput = new Label("1. the 57\r\n"
-					+ "2. and 38\r\n"
-					+ "3. i 32\r\n"
-					+ "4. my 24\r\n"
-					+ "5. of 22\r\n"
-					+ "6. that 18\r\n"
-					+ "7. this 16\r\n"
-					+ "8. a 15\r\n"
-					+ "9. door 14\r\n"
-					+ "10. is 11\r\n"
-					+ "11. raven 11\r\n"
-					+ "12. nevermore 11\r\n"
-					+ "13. chamber 11\r\n"
-					+ "14. bird 10\r\n"
-					+ "15. on 10\r\n"
-					+ "16. me 9\r\n"
-					+ "17. at 8\r\n"
-					+ "18. by 8\r\n"
-					+ "19. with 8\r\n"
-					+ "20. or 8");
+			primaryStage.setTitle("Text Analyzer - The Raven");
+			button = new Button();
+			button.setText("Calculate Word Count");
 			
-			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("Sample.fxml"));
-			Scene scene = new Scene(root,800,800);
-			root.getChildren().add(labelOutput);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			button.setOnAction(new EventHandler<ActionEvent>() {
+			    @Override
+			    public void handle(ActionEvent event) {
+			   
+			    	String text = "";  // variable to store the poem
+					
+					Path fileName = Path.of("poem.txt");
+					try {
+						text = Files.readString(fileName);
+					} catch (IOException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+					
+					// cleaning the data to get an accurate word count by
+					// removing punctuation and changing everything to lowercase 
+					
+					String[] textArray = text.toLowerCase().split("\\W+");  	
+
+					// creating three collections 
+					// 2 hashmaps - one to get frequency of each word - one to hold the sorted words
+					// 1 list to do the sorting/ranking top to bottom
+					
+					ArrayList<Integer> list = new ArrayList<>();
+				    Map<String, Integer> freqMap = new HashMap<>();
+				    LinkedHashMap<String, Integer> sortedMap = new LinkedHashMap<>();
+				    
+				    // loop to iterate through clean data 
+				    // if word exists - ups the count by 1
+				    // if word doesn't exist - adds word and sets count to 1
+				    for (String s : textArray)
+				    {	    	
+				        if (freqMap.containsKey(s)) {
+				            Integer count = freqMap.get(s);
+				            freqMap.put(s, count + 1);
+				        } else {
+				            freqMap.put(s, 1);
+				        }
+
+				    }
+				    // adds values to list
+			        for (Map.Entry<String, Integer> entry : freqMap.entrySet()) {
+			            list.add(entry.getValue());
+			        }
+			        // sorts list
+			        Collections.sort(list, new Comparator<Integer>() {
+			            public int compare(Integer str, Integer str1) {
+			                return (str1).compareTo(str);
+			            }
+			        });
+			        // uses list and keys from sorted data and
+			        // adds to new hashtable
+			        for (Integer str : list) {
+			            for (Map.Entry<String, Integer> entry : freqMap.entrySet()) {
+			                if (entry.getValue().equals(str)) {
+			                    sortedMap.put(entry.getKey(), str);
+			                }
+			            }
+			        }
+			        // iterates through sorted has table and outputs word and count
+			        // from highest count to lowest - top 20
+			        int count = 1;  // variable to count to 20
+			        for (Map.Entry<String, Integer> e: sortedMap.entrySet())
+			        {
+			        	
+			        	if (count < 21)
+			        	{
+			            textArea.appendText(count + ". " + e.getKey() + " " + e.getValue()+"\n");    	
+			        	System.out.println(count + ". " + e.getKey() + " " + e.getValue());
+			        	count += 1;
+			        	}
+
+			        }
+
+			    }
+			});
+			
+			
+			textArea = new TextArea();
+			textArea.setPrefHeight(700);
+			FlowPane layout = new FlowPane();
+			layout.getChildren().add(button);
+			layout.getChildren().add(textArea);
+			Scene scene = new Scene(layout,400,400);
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch(Exception e) {
